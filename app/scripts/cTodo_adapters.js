66a6876fd7150c4da99b8f2458adba8e42e921ec
clydeli <clyde1008li@gmail.com>
2013-02-12 16:35:43 -0800

    Google oauth testing

--- app/scripts/cTodo_adapters.js
+++ app/scripts/cTodo_adapters.js
@@ -2,12 +2,35 @@ var cTodo = cTodo || {};
 cTodo.Adapters = {};
 
 cTodo.Adapters.Google = {
-	// Adapter variables
-	listId : "",
-	updated : "",
-	idTable : {}, // A table for matching local Id to Google tasks Id
+	// cTodo registered info
+	regInfo : {
+		clientId : "207999260172.apps.googleusercontent.com",
+		apiKey : 'AIzaSyAZOeBh5jP4UnYQyV2cZYjD69hwUIYxK3s',
+		scope : 'https://www.googleapis.com/auth/tasks'
+	},
+	// User specific info
+	userInfo : {
+		listId : "",
+		updated : "",
+		idTable : {} // A table for matching local Id to Google tasks Id
+	},
 
-	// Adapter functions
+	//
+	auth : function(){
+		var config = {
+			'client_id': this.regInfo.clientId,
+			'scope': this.regInfo.scope
+		};
+		gapi.auth.authorize(config, function() {
+			console.log('login complete');
+			console.log(gapi.auth.getToken());
+		});
+    },
+
+	// Global adapter functions
+	initialize : function(){
+		gapi.client.setApiKey(this.regInfo.apiKey);
+	},
 	createTask : function(task, callback){
 
 	},
