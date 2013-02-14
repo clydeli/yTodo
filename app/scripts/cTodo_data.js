c0389628031c0038127817f1a4f389a5df9ffe17
clydeli <clyde1008li@gmail.com>
2013-02-14 00:54:39 -0800

    Complete basic local storage functions

--- app/scripts/cTodo_data.js
+++ app/scripts/cTodo_data.js
@@ -6,29 +6,12 @@ cTodo.Data = {
 	localTasks : {},
 
 	// Data functions
-	load : function(){ // Initialize Data with localStorage when possible
-		if (!(('localStorage' in window) && window.localStorage !== null)) { return false; }
-		var storedEntry;
-		for( storedEntry in localStorage){
-			if(/^task_/.test(storedEntry)){
-				this.localTasks[storedEntry.slice(5)] = JSON.parse(localStorage[storedEntry]);
-			}
-		}
-	},
-	save : function(taskId){ // Save Data with localStorage when possible
-		if (!(('localStorage' in window) && window.localStorage !== null)) { return false; }
-		if(taskId === undefined){ // Save all local tasks
-			var localTaskId;
-			for( localTaskId in this.localTasks){
-				localStorage["task_"+localTaskId] = JSON.stringify(this.localTasks[localTaskId]);
-			}
-		} else{ localStorage["task_"+taskId] = JSON.stringify(this.localTasks[taskId]); }
-	},
 	createTask : function(task){
 		// Generate a random Id (until no Id clash) for the task
 		do { task.id = (((1+Math.random())*0x10000)|0).toString(16).slice(1);
 		} while( this.localTasks.hasOwnProperty(task.id) )
 		this.localTasks[task.id] = task;
+		this.localSave(taskId);
 	},
 	getTask : function(taskId){
 		if(!this.localTasks.hasOwnProperty(taskId)){ return false; }
@@ -36,11 +19,34 @@ cTodo.Data = {
 	},
 	updateTask : function(taskId, partialTask){
 		if(!this.localTasks.hasOwnProperty(taskId)){ return false; }
-		var key;
-		for(key in partialTask){ this.localTasks[taskId][key] = partialTask[key]; }
+		for(var key in partialTask){ this.localTasks[taskId][key] = partialTask[key]; }
+		this.localSave(taskId);
 	},
 	deleteTask : function(taskId){
 		if(!this.localTasks.hasOwnProperty(taskId)){ return false; }
 		delete this.localTasks[taskId];
+		this.storageDelete(taskId);
+	},
+
+	// HTML5 Local Storage Functions
+	storageLoad : function(){ // Initialize Data with localStorage when possible
+		if (!(('localStorage' in window) && window.localStorage !== null)) { return false; }
+		for(var storedTaskKeys in localStorage){
+			if(/^task_/.test(storedTaskKeys)){
+				this.localTasks[storedTaskKeys.slice(5)] = JSON.parse(localStorage[storedTaskKeys]);
+			}
+		}
+	},
+	storageSave : function(taskId){ // Save Data with localStorage when possible
+		if (!(('localStorage' in window) && window.localStorage !== null)) { return false; }
+		if(taskId === undefined){ // Save all local tasks
+			for(var localTaskId in this.localTasks){
+				localStorage["task_"+localTaskId] = JSON.stringify(this.localTasks[localTaskId]);
+			}
+		} else{ localStorage["task_"+taskId] = JSON.stringify(this.localTasks[taskId]); }
+	},
+	storageDelete : function(taskId){
+		if (!(('localStorage' in window) && window.localStorage !== null)) { return false; }
+		delete localStorage["task_"+taskId];
 	}
 };
